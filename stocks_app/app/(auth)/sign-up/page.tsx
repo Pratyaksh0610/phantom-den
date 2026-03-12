e7fe5e9a9a12cf179aee60e72af1fa3468e544d4
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2026-03-12 08:35:57 +0530

    add authentication UI & layout (#4)
    * add authentication UI & layout
    
    * ui-fixes-1
    
    * review changes

---
+++ stocks_app/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,146 @@
+"use client";
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import { Button } from "@/components/ui/button";
+import {
+  emailRegex,
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
+import { useForm } from "react-hook-form";
+
+export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title"> Sign Up & Personalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{
+            required: {
+              value: true,
+              message: "Full name is mandatory",
+            },
+            minLength: {
+              value: 2,
+              message: "Minimum length should be greater than 2",
+            },
+          }}
+        />
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="johndoe@email.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: {
+              value: true,
+              message: "Email is mandatory",
+            },
+            pattern: {
+              value: emailRegex,
+              message: "Please enter a valid Email",
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="Enter a strong password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: {
+              value: true,
+              message: "Password is mandatory",
+            },
+            minLength: {
+              value: 8,
+              message: "Length should be at least 8",
+            },
+          }}
+        />
+        <CountrySelectField
+          name="country"
+          label="Country"
+          control={control}
+          error={errors.country}
+          required
+        />
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goal"
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+        <SelectField
+          name="riskTolerance"
+          label="Risk Tolerance"
+          placeholder="Select your risk level"
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          error={errors.riskTolerance}
+          required
+        />
+        <SelectField
+          name="preferredIndustry"
+          label="Preferred Industry"
+          placeholder="Select your preferred industry"
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          error={errors.preferredIndustry}
+          required
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating Account" : "Start your Investing Journey"}
+        </Button>
+        <FooterLink
+          text="Already have an account"
+          linkText="Sign in"
+          href="/sign-in"
+        />
+      </form>
+    </>
+  );
+}
---
+++ stocks_app/components/forms/SelectField.tsx
@@ -0,0 +1,53 @@
+import { Controller, FieldValues } from "react-hook-form";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function SelectField<T extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  options,
+  control,
+  error,
+  required = false,
+}: SelectFieldProps<T>) {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor={name} className="form-label">
+        {label}
+      </Label>
+      <Controller
+        name={name}
+        control={control}
+        rules={{
+          required: required ? `Please select ${label.toLowerCase()}` : false,
+        }}
+        render={({ field }) => (
+          <Select value={field.value} onValueChange={field.onChange}>
+            <SelectTrigger className="select-trigger" id={name}>
+              <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent className="bg-gray-800 border-gray-600 text-white">
+              {options.map((option) => (
+                <SelectItem
+                  value={option.value}
+                  key={option.value}
+                  className="focus:bg-gray-600 focus:text-white"
+                >
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+            {error && <p className="text-sm text-red-500">{error.message}</p>}
+          </Select>
+        )}
+      />
+    </div>
+  );
+}
