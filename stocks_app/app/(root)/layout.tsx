18d2b89fa079561e66879c3c30433445e933f4b4
Pratyaksh <pratyaksh0610@gmail.com>
2026-03-13 14:13:09 +0530

    add authenticaion logic with inngest integration

--- stocks_app/app/(root)/layout.tsx
+++ stocks_app/app/(root)/layout.tsx
@@ -1,10 +1,24 @@
 import Header from "@/components/Header/Header";
+import { auth } from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Layout(props: { children: React.ReactNode }) {
+export default async function Layout(props: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if(!session?.user) redirect('/sign-in');
+
+  const user = {
+    id: session.user.id,
+    email: session.user.email,
+    name: session.user.name,
+  }
+
   return (
     <main className="min-h-screen text-gray-400">
-      <Header/>
+      <Header user={user} />
       <div className="container py-10">{props.children}</div>
     </main>
   );
---
+++ stocks_app/lib/actions/auth-actions.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "../better-auth/auth";
+import { inngest } from "../inngest/client";
+
+export const signUpWithEmail = async ({
+  email,
+  password,
+  fullName,
+  country,
+  investmentGoals,
+  riskTolerance,
+  preferredIndustry,
+}: SignUpFormData) => {
+  try {
+    const response = await auth.api.signUpEmail({
+      body: { email: email, password: password, name: fullName },
+    });
+
+    if (response) {
+      await inngest.send({
+        name: "app/user.created",
+        data: {
+          email: email,
+          name: fullName,
+          country: country,
+          investmentGoals: investmentGoals,
+          riskTolerance: riskTolerance,
+          preferredIndustry: preferredIndustry,
+        },
+      });
+    }
+    return { success: true, data: response };
+  } catch (e) {
+    console.log("Sign up failed", e);
+    return { success: false, message: "Sign up failed. Please try again." };
+  }
+};
+
+export const signInWithEmail = async ({
+  email,
+  password
+}: SignInFormData) => {
+  try {
+    const response = await auth.api.signInEmail({
+      body: { email: email, password: password },
+    });
+
+    return { success: true, data: response };
+  } catch (e) {
+    console.log("Sign in failed", e);
+    return { success: false, message: "Sign in failed. Please try again." };
+  }
+};
+
+export const signOut = async () => {
+  try {
+    await auth.api.signOut({ headers: await headers() });
+  } catch (e) {
+    console.log("Sign out failed", e);
+    return { success: false, message: "Sign out failed. Please try again." };
+  }
+};
